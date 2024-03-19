f9c5ac403caaf41c5b8c7de8c7787ee87dcf0f51
Support-Bots-OFC <155838423+Bots-WhatsApp-OFC@users.noreply.github.com>
2024-03-18 22:36:39 -0300

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,12 +1,12 @@
 import { execSync } from 'child_process'
 
 var handler = async (m, { conn, text }) => {
-await conn.sendMessage(m.chat, { react: { text: '🍭', key: m.key } })
+await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
 try {
 const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
 let messager = stdout.toString()
-if (messager.includes('Already up to date.')) messager = '🍭 *No Hay Actualizaciones Pendientes.*'
-if (messager.includes('Updating')) messager = '✅️ *Actualizacion Exitosa.*\n\n' + stdout.toString()
+if (messager.includes('Already up to date.')) messager = '> ⓘ *No hay actualizaciones aun...*'
+if (messager.includes('Updating')) messager = '> ✅️ *ACTUALIZACION EXITOSA* ✅\n\n' + stdout.toString()
 conn.reply(m.chat, messager,)
 } catch { 
 try {
@@ -18,13 +18,13 @@ return null
 }
 return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
 if (conflictedFiles.length > 0) {
-const errorMessage = `🧸 *Se Han Hecho Cambios Locales En Archivos Del Bot Que Entran En Conflicto Con Las Actualizaciones Del Repositorio. Para Actualizar, Reinstala El Bot o Realiza Las Actualizaciones Manualmente*\n\nArchivos En Conflicto:\n\n${conflictedFiles.join('\n')}`
+const errorMessage = `> ✅ *Se han encontrado archivos en conflicto, para esto hay que volver a instalar el bot.*\n\n> \`ARCHIVOS EN CONFLICTO:\`\n\n${conflictedFiles.join('\n')}`
 await conn.reply(m.chat, errorMessage,)
 }
 }
 } catch (error) {
 console.error(error)
-let errorMessage2 = '📍 *Ocurrió Un Error.*'
+let errorMessage2 = '> ⓘ *Ocurrio un error inesperado, vuelva a intentarlo.*'
 if (error.message) {
 errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
 }
