4966555c0548bd4e9d4c7c3d1b4d3250a0b334b1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-24 18:36:07 -0500

    Actualizar downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,58 +1,40 @@
+
+
 import fetch from 'node-fetch';
-import axios from 'axios';
-import instagramGetUrl from 'instagram-url-direct';
-import {instagram} from '@xct007/frieren-scraper';
-import {instagramdl} from '@bochilteam/scraper';
-const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!args[0]) throw `> ⓘ 𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙚𝙣𝙡𝙖𝙘𝙚 𝙙𝙚 𝙞𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢 𝙫𝙖𝙡𝙞𝙙𝙤, 𝙥𝙤𝙧 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n> *${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link`;
-  m.reply(`> ⓘ 𝙎𝙚 𝙚𝙨𝙩𝙖 𝙙𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙣𝙙𝙤 𝙚𝙡 𝙫𝙞𝙙𝙚𝙤, 𝙚𝙨𝙥𝙚𝙧𝙚 𝙪𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤 𝙥𝙤𝙧 𝙛𝙖𝙫𝙤𝙧.`);
-  try {
-    const apiUrll = `https://api.betabotz.org/api/download/igdowloader?url=${encodeURIComponent(args[0])}&apikey=bot-secx3`;
-    const responsel = await axios.get(apiUrll);
-    const resultl = responsel.data;
-    for (const item of resultl.message) {
-      const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${item.thumbnail}`)).text();
-      const tXXxt = `> ⓘ 𝙑𝙞𝙙𝙚𝙤 𝙙𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙙𝙤 𝙙𝙚:\n${shortUrRRl}`.trim();
-      conn.sendFile(m.chat, item._url, null, tXXxt, m);
-      await new Promise((resolve) => setTimeout(resolve, 10000));
+
+const handler = async (m, { conn, args }) => {
+
+    if (!args[0]) {
+        throw `Por favor, ingresa un enlace de Instagram para descargar el archivo.`;
     }
-  } catch {    
-  try {
-    const datTa = await instagram.v1(args[0]);
-    for (const urRRl of datTa) {
-      const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-      const tXXxt = `> ⓘ 𝙑𝙞𝙙𝙚𝙤 𝙙𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙙𝙤 𝙙𝙚:\n${shortUrRRl}`.trim();
-      conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m);
-      await new Promise((resolve) => setTimeout(resolve, 10000));
-    }
-  } catch {
-      try {
-        const resultss = await instagramGetUrl(args[0]).url_list[0];
-        const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-        const txt2 = `> ⓘ 𝙑𝙞𝙙𝙚𝙤 𝙙𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙙𝙤 𝙙𝙚:\n${shortUrl2}`.trim();
-        await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m);
-      } catch {
-        try {
-          const resultssss = await instagramdl(args[0]);
-          const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-          const txt4 = `> ⓘ 𝙑𝙞𝙙𝙚𝙤 𝙙𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙙𝙤 𝙙𝙚:\n${shortUrl3}`.trim();
-          for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m);
-        } catch {
-          try {
-            const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`);
-            const json = await human.json();
-            const videoig = json.result;
-            const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-            const txt1 = `> ⓘ 𝙑𝙞𝙙𝙚𝙤 𝙙𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙙𝙤 𝙙𝙚:\n${shortUrl1}`.trim();
-            await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m);
-          } catch {
-            throw `> ⓘ 𝙊𝙘𝙪𝙧𝙧𝙞𝙤 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧 𝙞𝙣𝙚𝙨𝙥𝙚𝙧𝙖𝙙𝙤, 𝙥𝙤𝙧 𝙛𝙖𝙫𝙤𝙧, 𝙫𝙪𝙚𝙡𝙫𝙖 𝙖 𝙞𝙣𝙩𝙚𝙣𝙩𝙖𝙧𝙡𝙤.`;
-          }
+
+    try {
+        const apiUrl = `https://aemt.me/download/igdl?url=${encodeURIComponent(args[0])}`;
+        const response = await fetch(apiUrl);
+        const responseData = await response.json();
+
+        m.react(rwait);
+
+        if (responseData.status && responseData.result.length > 0) {
+            for (const media of responseData.result) {
+                m.react(done);
+                await conn.sendFile(m.chat, media.url, media.url.includes('.mp4') ? 'video.mp4' : 'imagen.jpg', '', m);
+            }
+        } else {
+            throw `error
+
+No se pudo obtener el contenido de Instagram.`;
         }
-      }
+    } catch (error) {
+        console.error(error);
+        throw `error
+
+Ocurrió un error al procesar la solicitud: ${error.message}`;
     }
-  }
 };
-handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
-handler.register = true
+
+handler.help = ['instagram'];
+handler.tags = ['dl'];
+handler.command = /^(instagramdl|instagram|igdl|ig)$/i;
+
 export default handler;
